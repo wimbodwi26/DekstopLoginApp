ad981569393f7889f269eb08f1ef4b5bc89350d4
wimbodwi26 <wimbodwin@gmail.com>
2025-12-03 14:18:45 +0700

    new file

---
+++ WinFormsApp1/RegisterForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WinFormsApp1
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox4 = new TextBox();
+            button2 = new Button();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += this.button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(95, 61);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(312, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(95, 116);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(312, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(95, 168);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(312, 23);
+            textBox4.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(95, 275);
+            button2.Name = "button2";
+            button2.Size = new Size(118, 23);
+            button2.TabIndex = 7;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(95, 220);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(312, 23);
+            textBox3.TabIndex = 8;
+            // 
+            // RegisterForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox3);
+            Controls.Add(button2);
+            Controls.Add(textBox4);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "RegisterForm";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox4;
+        private Button button2;
+        private TextBox textBox3;
+    }
+}
---
+++ WinFormsApp1/RegisterForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class RegisterForm : Form
+    {
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+            {
+            Form1 form1 = new Form1();
+            form1.Show();
+            this.Hide();
+            }
+    }
+}
